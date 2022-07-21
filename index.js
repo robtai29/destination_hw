8cce2088d1e3354fe6d9036eefa92f3892207c71
Robert Tai <tairobea@amazon.com>
2022-07-20 19:47:24 -0700

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 
 const server = express(); // This server is deaf
-const port = process.env.PORT;
-server.listen(3000); // Told the server to listen on port 3000
+const port = process.env.PORT || 3000;
+server.listen(port); // Told the server to listen on port 3000
 
 const destinationsDB = {
   123456: {
