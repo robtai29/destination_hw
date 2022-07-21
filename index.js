2d54d864d89e25c802b65849c731d3216488eb3e
Robert Tai <tairobea@amazon.com>
2022-07-20 19:40:10 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+const server = express(); // This server is deaf
+const port = process.env.PORT;
+server.listen(3000); // Told the server to listen on port 3000
+
+const destinationsDB = {
+  123456: {
+    destination: "Eiffel Tower",
+    location: "Paris",
+    photo:
+      "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  },
+  234567: {
+    destination: "Big Ben",
+    location: "London",
+    photo:
+      "https://images.unsplash.com/photo-1529655683826-aba9b3e77383?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
+  },
+};
+
+server.get("/", (req, res) =>{
+    var myJSON = JSON.stringify(destinationsDB);
+    res.send(myJSON);
+});
